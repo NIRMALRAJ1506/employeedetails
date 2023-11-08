63a73c12d12a97f8c5768659e02e95bcfa75fab5
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2023-11-08 14:28:13 +0530

    First Commit

---
+++ Employeedetails/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Employeedetails
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Employee ID");
+            int id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the Employeename");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the Employeegender");
+            string gender = Console.ReadLine();
+            Console.WriteLine("Enter Salary");
+            double salary = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter Date of Joining");
+            DateTime doj = DateTime.Parse(Console.ReadLine());
+            if (salary > 90000)
+            {
+                Console.WriteLine("You have to pay 30% tax");
+            }
+            else
+            {
+                Console.WriteLine("You have to pay 15% tax");
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
