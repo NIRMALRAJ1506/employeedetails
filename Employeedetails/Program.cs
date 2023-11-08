3ee52c30fa0157e377680353c8dc3fdb8a180c3b
NIRMALRAJ1506 <nirmalraj75500@gmail.com>
2023-11-08 14:53:19 +0530

    Second commit

--- Employeedetails/Program.cs
+++ Employeedetails/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace Employeedetails
 {
@@ -20,6 +21,8 @@ namespace Employeedetails
             double salary = double.Parse(Console.ReadLine());
             Console.WriteLine("Enter Date of Joining");
             DateTime doj = DateTime.Parse(Console.ReadLine());
+            Console.WriteLine("ID\tEmployee Name\tEmployee Gender\tEmployee Salary\t\tDOJ");
+            Console.WriteLine($"{id}\t{name}\t\t{gender}\t\t{salary:C2}\t\t{doj:MM/dd/yyyy}");
             if (salary > 90000)
             {
                 Console.WriteLine("You have to pay 30% tax");
